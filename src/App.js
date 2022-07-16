ae66df6fc8a10c05f0e5b13f9bf465401649080b
Sheila Anguiano <sheila.anguiano@gmail.com>
2022-07-16 00:40:18 -0700

    Refactored Nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import {
 	BrowserRouter,
+  Routes,
 	Route
 } from  'react-router-dom';
 
@@ -20,19 +21,38 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      photos:[]
+      photos:[],
+      sculpture:[],
+      architecture: [],
+      painting: [],
+      loading: true
     }
   }
 
-  componentDidMount(){
+  componentDidMount(){  
+    this.performSearch('sculpture');
+    this.performSearch('architecture');
+    this.performSearch('painting');
     this.performSearch();
   }
   
-  performSearch = (query = 'pen and ink') => {
+  performSearch = (query = 'pomeranians') => {
     fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(response => response.json())
       .then(responseData => {
-        this.setState({photos : responseData.photos.photo })
+        if(query === 'sculpture'){
+          this.setState({sculpture: responseData.photos.photo, loading: false });
+        } else if (query === 'architecture'){
+          this.setState({architecture: responseData.photos.photo, loading: false });
+        } else if (query === 'painting'){
+          this.setState({painting: responseData.photos.photo, loading: false });
+        } else {
+          this.setState({
+            photos : responseData.photos.photo,
+            query: query,
+            loading: false
+           })
+        }        
       })
       .catch(error => {
         console.log('Error fectching and passing data', error);
@@ -40,15 +60,26 @@ class App extends Component {
 
   }
 
+
   render (){
     return (
       <BrowserRouter>
         <div className="container">
           <h1>Welcome to Photo App</h1>, 
-            <SearchForm onSearch={this.performSearch} />,
-            <Nav />,
-            {/* <Route> <Route/> */}
-            <PhotoList data={this.state.photos} />
+
+          <SearchForm onSearch={this.performSearch} />,
+          
+          <Nav />,
+          <Routes>
+            <Route  exact path='/' element={<PhotoList data={this.state.photos} />} />
+            <Route  exact path='/sculpture' element={<PhotoList data={this.state.sculpture} />} />
+            <Route  exact path='/architecture' element={<PhotoList data={this.state.architecture} />} />
+            <Route  exact path='/painting' element={<PhotoList data={this.state.painting} />} />
+            <Route  path='/search/:query' element={<PhotoList data={this.state.photos} query={this.state.query} />} />
+
+                
+          </Routes>
+          
         </div>
       </BrowserRouter>
     );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import { NavLink } from  'react-router-dom';
 
 //Stateles Component
 
-const Nav = () => {
+const Nav = (props) => {
     return (
         <nav className="main-nav">
             <ul>
-                <li><a href='www.google.com'>Sculpture</a></li>
-                <li><a href='www.google.com'>Architecture</a></li>
-                <li><a href='www.google.com'>Painting</a></li>
+                <li>
+                    <NavLink
+                        to='/sculpture' 
+                        // onClick={props.queryString}
+                        // id='sculpture'
+                        >
+                            Sculpture  
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/architecture' 
+                        // onClick={props.queryString}
+                        // id='architecture'
+                        >
+                            Architecture 
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/painting' 
+                        // onClick={props.queryString}
+                        // id='painting'
+                        >
+                            Painting  
+                    </NavLink>
+                </li>
             </ul>
       </nav>
     );
