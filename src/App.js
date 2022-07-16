aa2b5ead433b8610b56555e316d7fb3e96e01976
Sheila Anguiano <sheila.anguiano@gmail.com>
2022-07-16 00:39:03 -0700

    button routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+	BrowserRouter,
+	Route
+} from  'react-router-dom';
+
+
+//Components
+import apiKey from './config';
 import Nav from './components/Nav';
 import PhotoList from './components/PhotoList';
+import SearchForm from './components/SearchForm';
 
 
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello to my App</h1>,
-      <Nav />,
-      <PhotoList />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      photos:[]
+    }
+  }
+
+  componentDidMount(){
+    this.performSearch();
+  }
+  
+  performSearch = (query = 'pen and ink') => {
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({photos : responseData.photos.photo })
+      })
+      .catch(error => {
+        console.log('Error fectching and passing data', error);
+      });
+
+  }
+
+  render (){
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <h1>Welcome to Photo App</h1>, 
+            <SearchForm onSearch={this.performSearch} />,
+            <Nav />,
+            {/* <Route> <Route/> */}
+            <PhotoList data={this.state.photos} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
